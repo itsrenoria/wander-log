26e7a36b6f0d399a458cd8ea6179d8cb3853c1aa
itsrenoria <itsrenoria@users.noreply.github.com>
2025-11-19 16:59:51 +0400

    Initial release of Wander Log 2.0

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'wander-log-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './sri-lanka/index.html',
+  './thailand-vietnam/index.html',
+  './icon.jpg',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
